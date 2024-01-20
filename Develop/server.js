0662c7f4e11b9633656d7b9353c9ed435f3f7614
Dani Drury <danidrury98@gmail.com>
2024-01-20 15:00:54 -0800

    initial commit

---
+++ Develop/server.js
@@ -0,0 +1,31 @@
+// acquire express tools
+const express = require('express');
+const path = require('path');
+
+// create routes variables
+const api = require('./routes/index.js')
+
+// set defult PORT
+const PORT = process.env.PORT || 3001;
+
+// initialize express
+const app = express();
+
+// middleware
+// for handling json data requests
+app.use(express.json());
+// for handling url requests
+app.use(express.urlencoded({ extended: true }));
+// for handling api requests
+app.use('/api', api);
+// for setting static requests
+app.use(express.static('public'));
+
+// GET route for homepage
+app.get('/', (req, res) =>
+	res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+	console.log(`App listening at http://localhost:${PORT}`)
+);
