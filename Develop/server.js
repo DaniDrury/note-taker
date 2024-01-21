c3b6e715f65b11c9541e05496a3cb7d7083d6356
Dani Drury <danidrury98@gmail.com>
2024-01-20 17:39:42 -0800

    missed save on server.js after route update

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 // create routes variables
-const api = require('./routes/index.js');
+const api = require('./routes/routes.js');
 
 // set defult PORT
 const PORT = process.env.PORT || 3001;
